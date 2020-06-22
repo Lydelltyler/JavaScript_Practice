0b9bdfa402418621cf0c05041236e346caf68abc
lydelltyler <lydellgrowen@gmail.com>
2020-06-22 05:19:15 -0500

    Need to go to sleep but more reps

--- Basic_Foundation_2.js
+++ Basic_Foundation_2.js
@@ -72,3 +72,154 @@ function countPositives(arr) {
 
 // 6. Evens and Odds 
 
+var after3odd = 0;
+var after3even = 0;
+
+function evenOdd(arr) {
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            after3odd += 1;
+        }
+        if (after3odd == 4) {
+            arr[i] = "That's odd!";
+            after3odd = 0;
+        }
+        if (arr[i] % 2 == 0) {
+            after3even += 1;
+        }
+        if (after3even == 4) {
+            arr[i] = "Even more so!";
+            after3even = 0;
+        }
+
+    }
+
+    return arr;
+}
+
+// 7. increment the Seconds
+
+
+function seconds(arr) {
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i % 2 !== 0) {
+            arr[i] += 1;
+        }
+        console.log(arr[i])
+    }
+
+    return arr;
+}
+
+//8. Previous Lengths
+
+function prev_string(arr) {
+
+    for (var i = arr.length - 1; i > 0; i--) {
+        arr[i] = arr[i - 1].length;
+    }
+
+    return arr;
+}
+
+// 9. Add Seven
+
+function addSeven(arr) {
+
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] += 7
+    }
+
+    return arr;
+}
+
+// 10. Reverse Array 
+
+function reverse(arr) {
+
+    var x = 0;
+    var y = 1;
+
+
+    for (var i = 0; i < arr.length; i++) {
+        var temp = arr[x];
+        if (i <= ((arr.length - 2) / 2)) {
+            arr[i] = arr[arr.length - y]
+            arr[arr.length - y] = temp
+            y++
+            x++
+            console.log(temp)
+            console.log(x)
+        }
+    }
+
+    return arr;
+
+}
+
+// 11. Outlook: Negative
+
+function negative(arr) {
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            arr[i] *= -1
+        }
+    }
+
+    return arr
+
+}
+
+// 12. Always Hungry
+
+function hungry(arr) {
+
+    var foodCheck = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == "food") {
+            console.log("yummy")
+            foodCheck += 1
+        }
+    }
+
+    if (foodCheck == 0) {
+        console.log("im hungry");
+    }
+
+    return arr
+
+}
+
+// 13. Swap Toward the Center
+
+function towardCenter(arr) {
+
+    var temp = arr[0]
+    var temp2 = arr[2]
+
+    arr[0] = arr[arr.length - 1];
+    arr[arr.length - 1] = temp
+
+    arr[2] = arr[arr.length - 3];
+    arr[arr.length - 3] = temp2
+
+
+    return arr;
+
+}
+
+// 14. Scale the Array
+
+function scaleArray(arr, num) {
+
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] *= num
+    }
+
+    return arr;
+
+}
